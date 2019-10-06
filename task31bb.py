6b4b2d1bea4f90df5c829c0ebd691571d1268cfa
Tvorec114 <ols114kiev@gmail.com>
2019-10-06 19:25:53 +0300

    first

---
+++ task31bb.py
@@ -0,0 +1,8 @@
+a = input('Введите А ')
+a = int(a)
+b = a * a
+c = b * b
+b = c * b 
+a = a * b
+
+print(a)
---
+++ task8.py
@@ -0,0 +1,7 @@
+n = input('Введите n = ')
+r = input('Введите r = ')
+n = int(n)
+r = int(r)
+import math
+p = 2*r*n*math.sin(3.14/n)
+print(p)
